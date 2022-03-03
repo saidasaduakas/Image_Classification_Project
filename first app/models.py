b87f6838521c5f710b3c7e95ca47b954f631b68f
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2022-03-04 02:54:08 +0600

    Update models.py

--- first app/models.py
+++ first app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class ImageDB(models.Model):
-    paths = models.CharField(max_length=100)
+    pathh = models.CharField(max_length=100)
     # # pathh = models.CharField(max_length=100)
     des = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.address} - {self.balance}'
+        return f'{self.pathh} - {self.des}'
 
