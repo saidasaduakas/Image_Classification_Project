79548bf6a821b83eeae79b5d821c1657925c83c5
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2022-03-04 02:55:14 +0600

    Update views.py

--- first app/views.py
+++ first app/views.py
@@ -4,7 +4,9 @@ from django.core.files.storage import FileSystemStorage
 from keras.models import load_model
 from keras.preprocessing import image
 import json
-from .models import ImageDB
+from . models import ImageDB
+
+
 
 img_height, img_width = 224, 224
 with open('./models/imagenet_classes.json', 'r') as f:
@@ -12,6 +14,7 @@ with open('./models/imagenet_classes.json', 'r') as f:
 
 labelInfo = json.loads(labelInfo)
 model = load_model('./models/MobileNetModelImagenet.h5')
+
 
 
 def index(request):
@@ -26,22 +29,25 @@ def predictImage(request):
     fs = FileSystemStorage()
     filePathName = fs.save(fileObj.name, fileObj)
     filePathName = fs.url(filePathName)
-    testimage = '.' + filePathName
+    testimage = '.'+filePathName
     img = image.load_img(testimage, target_size=(img_height, img_width))
     x = image.img_to_array(img)
-    x = x / 255
+    x = x/255
     x = x.reshape(1, img_height, img_width, 3)
     predi = model.predict(x)
     import numpy as np
     predictedLabel = labelInfo[str(np.argmax(predi[0]))]
-    context = {'filePathName': filePathName, 'predictedLabel': predictedLabel}
-    ImageDB.objects.create(paths=filePathName, des=predictedLabel)
-
+    context= {'filePathName': filePathName,'predictedLabel': predictedLabel}
+    ImageDB.objects.create(pathh=filePathName, des = predictedLabel)
+    
     return render(request, 'index.html', context)
 
 
 def viewDataBase(request):
     listofImages = os.listdir('./media/')
     listofImagesPath = ['./media/' + i for i in listofImages]
-    context = {'listofImagesPath': listofImagesPath}
+    data = ImageDB.objects.all()
+    context = {'listofImagesPath': listofImagesPath, "images": data }
+    
+    
     return render(request, 'viewDB.html', context)
